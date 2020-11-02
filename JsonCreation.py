b6807a57b162a3632e432578e64764674aa7da78
橘 园 <281848209@qq.com>
2020-11-02 15:19:24 +0800

    JsonTools
    完成了自动生成Json文件的脚本（初级），目前只提供了方法，后续需要添加其他生成脚本

--- JsonCreation.py
+++ JsonCreation.py
@@ -1,18 +1,46 @@
 # 制作变量名的Json文件
+import re
+
+
 def createJson(eqName):
     """
     编写某个设备的Json文件
     :param eqName:设备的名字
     :return:
     """
-    with open("Names","r",encoding="UTF-8") as r,open("EQProperties.json","w",encoding="UTF-8") as w:
-        w.write("{\n")
-        w.write("\"{}\":{".format(eqName))
+    with open("Names", "r", encoding="UTF-8") as r, open("EQProperties.json", "w", encoding="UTF-8") as w:
+        w.write("\"%s\":{\n" % eqName)
         datas = r.readlines()
         for data in datas:
-
+            nameAndValue = data.split("/")
+            name = ""
+            value = ""
+            for item in nameAndValue:
+                item = item.replace(" ","").replace("\n","")
+                res = re.match("[a-zA-Z0-9_#]{1,}", item)
+                if is_Chinese(item):
+                    name = item
+                    pass
+                elif res:
+                    value = res.group()
+                pass
+            w.write("\"{}\": \"{}\",\n".format(name, value))
             pass
-        w.write("}")
         w.write("}\n")
         pass
     pass
+
+
+def is_Chinese(word):
+    """
+    判断字符是否为中文
+    :param word:
+    :return:
+    """
+    for ch in word:
+        if '\u4e00' <= ch <= '\u9fff':
+            return True
+    return False
+
+
+createJson("Feeder")
